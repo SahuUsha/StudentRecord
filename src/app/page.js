38fc31901aa4c1108d667d18ff0f9fe9551c85cc
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2024-10-06 22:47:29 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="min-h-screen flex justify-center items-center  bg-gradient-to-r from-gray-300 to-gray-400   "> 
     <div className="contianer mx-auto flex flex-col justify-center items-center ">
      <h2 className="text-4xl text-slate-800 font-bold mb-4">Add and Explore Students Record</h2>
-     <Link className="text-white text-xl bg-slate-800  py-2 font-bold px-6 rounded-lg" href={'/record'}>Get Started</Link>
+     <Link className="text-white text-xl bg-slate-800  py-2 font-bold px-6 rounded-lg" href={"/record"}>Get Started</Link>
     </div>
   </div>
 
