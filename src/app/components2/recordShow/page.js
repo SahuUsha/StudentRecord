d44bdb5f45402880d1b79616370aba6808a2e49e
Usha Sahu <155312236+SahuUsha@users.noreply.github.com>
2024-10-06 22:44:52 +0530

    Update page.js

--- src/app/components2/recordShow/page.js
+++ src/app/components2/recordShow/page.js
@@ -1,16 +1,16 @@
 "use client"
 
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import AddNewStudent from '../add-new-student/page'
+import { useRouter } from "next/navigation"
+import React, { useEffect } from "react"
+import { useState } from "react"
+import AddNewStudent from "../add-new-student/page"
 
 import {Card,
     CardContent,
     CardDescription,
-    CardTitle, } from '@/components/ui/card'
-  import { Button } from '@/components/ui/button'
-import { get } from 'mongoose'
+    CardTitle, } from "@/components/ui/card"
+  import { Button } from "@/components/ui/button"
+import { get } from "mongoose"
 
 
 const initialStudentFormData={
@@ -67,7 +67,7 @@ const handleSaveStudentRecord=async()=>{
 const handleDeleteRecordByID=async(getCurrentStudentId)=>{
     try{
         const apiResponse = await fetch(`/api/delete-student?id=${getCurrentStudentId}`,{
-             method : 'DELETE'
+             method : "DELETE"
         })
         const result = await apiResponse.json();
         if(result?.success){
@@ -107,8 +107,8 @@ const handleEditRecord=async(getCurrentStudent)=>{
      <div>
         <h3  className="text-3xl font-semibold text-center  text-4xl text-slate-800 ">Students Record</h3>
      </div>
-     <div className='flex justify-center items-center'>
-     <div className='flex flex-wrap w-auto shadow-xl shadow-gray-600 bg-gray-400 rounded-2xl p-12 gap-7 sm:grid-cols-2 justify-center flex   mt-6'>
+     <div className="flex justify-center items-center">
+     <div className="flex flex-wrap w-auto shadow-xl shadow-gray-600 bg-gray-400 rounded-2xl p-12 gap-7 sm:grid-cols-2 justify-center flex   mt-6">
         {
             studentList && studentList.length>0?
             studentList.map((item)=>
@@ -121,7 +121,7 @@ const handleEditRecord=async(getCurrentStudent)=>{
              
 
              
-             <div className='mt-5 flex gap-5  items-center'>
+             <div className="mt-5 flex gap-5  items-center">
              <Button className="cursor-pointer px-10 text-[1rem] text-white text-xl bg-slate-800 " onClick = {()=>handleEditRecord(item) }  >Edit</Button>
              <Button className="cursor-pointer px-10 text-[1rem] text-white text-xl bg-slate-800 " onClick={()=>handleDeleteRecordByID(item._id)}>Delete</Button>
              </div>
@@ -129,7 +129,7 @@ const handleEditRecord=async(getCurrentStudent)=>{
          </Card>
 
             )
-            :<label className='text-4xl text-white font-bold mb-4 flex justify-center items-center text-center '>No one Student's record Found. please add at least One student's record</label>
+            :<label className="text-4xl text-white font-bold mb-4 flex justify-center items-center text-center ">No one Students record Found. please add at least One students record</label>
         }
      </div>
      </div>
